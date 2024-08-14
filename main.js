d3b262cf9e94318b2bae643e7c23b07df70c2419
Porterisimo07 <167125663+Porterisimo07@users.noreply.github.com>
2024-08-14 12:58:17 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,70 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-var roverwidth=100;
-var roverheight=90;
-var roverx=10;
-var rovery=10;
+var roverwidth = 100;
+var roverheight = 90;
+var roverx = 10;
+var rovery = 10;
 var bg = new Image();
 var rover = new Image();
-function cargar(){
-    bg.onload=uploadbg;
-    bg.src="mars.jpg";
-    rover.onload=uploadrover;
-    rover.src="rover.png";
+function cargar() {
+    bg.onload = uploadbg;
+    bg.src = "mars.jpg";
+    rover.onload = uploadrover;
+    rover.src = "rover.png";
 }
-function uploadbg(){
-    ctx.drawImage(bg,0,0,canvas.width,canvas.height)
+function uploadbg() {
+    ctx.drawImage(bg, 0, 0, canvas.width, canvas.height)
 }
-function uploadrover(){
-    ctx.drawImage(rover,roverx,rovery,roverwidth,roverheight);
+function uploadrover() {
+    ctx.drawImage(rover, roverx, rovery, roverwidth, roverheight);
+}
+
+window.addEventListener("keydown", mover);
+function mover(t) {
+    console.log(t);
+    keypress = t.keyCode;
+    if (keypress == "38") {
+        up();
+    }
+    if(keypress=="40"){
+        down();
+    }
+    if(keypress=="39"){
+        right();
+    }
+    if(keypress=="37"){
+        left();
+    }
+}
+
+function up() {
+    if(rovery>=0){
+        rovery = rovery - 10;
+        uploadbg();
+        uploadrover();   
+    }
+}
+
+function down() {
+    if(rovery<=500){
+        rovery = rovery +10;
+        uploadbg();
+        uploadrover();   
+    }
+}
+
+function right() {
+    if(roverx<=700){
+        roverx = roverx +10;
+        uploadbg();
+        uploadrover();   
+    }
+}
+
+function left() {
+    if(roverx>=0){
+        roverx = roverx -10;
+        uploadbg();
+        uploadrover();   
+    }
 }
